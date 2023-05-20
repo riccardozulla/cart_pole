64147c564b480219981d5d681939c5de3ccf0c3b
riccardozulla <riccardozulla@gmail.com>
2023-05-20 15:07:49 +0200

    add total_reward

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 import torch
 
 
-EPISODES = 100
+EPISODES = 500
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 env = gym.make('CartPole-v1', render_mode="human")
 agent = DQNagent(env, 4, 2, torch.nn.HuberLoss(), device)
@@ -15,13 +15,15 @@ for n in range(EPISODES):
     state = torch.tensor(state, dtype=torch.float32,
                          device=device).unsqueeze(0)
     previousReward = None
-
+    total_reward = 0
     while True:
         action = env.action_space.sample()
         action = agent.step(state, previousReward, steps=steps)
         observation, reward, terminated, truncated, _ = env.step(action.item())
         observation = torch.tensor(
             observation, dtype=torch.float32, device=device).unsqueeze(0)  # voglio che abbia già implementata la dimensione dei batch
+        total_reward += reward
+
         reward = torch.tensor(reward, dtype=torch.float32,
                               device=device).unsqueeze(0)
         previousReward = reward
@@ -33,5 +35,6 @@ for n in range(EPISODES):
             break
 
         steps += 1
+    print(total_reward)
 
 env.close()
